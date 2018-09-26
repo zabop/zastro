00637b8f61aae930f1782e6340154934253a1b5d
Pal Szabo <szabopal96@gmail.com>
2018-09-26 19:45:51 +0200

    adding the function period(BJD,flux)

--- zastro/__init__.py
+++ zastro/__init__.py
@@ -17,3 +17,22 @@ def csvtodict(filename, **kwargs):
                 for INDEX, EACH in enumerate(headers): headers[EACH].append(float(splitline[INDEX]))
     return(headers)
 
+
+
+def period(BJD,flux,**kwargs):
+
+    from gatspy.periodic import LombScargleFast
+
+    if ('dmag' in kwargs): dmag=kwargs['dmag']
+    else: dmag=0.000005
+    if ('nyquist_factor' in kwargs): nyquist_factor=kwargs['nyquist_factor']
+    else: nyquist_factor=40
+    
+    model = LombScargleFast().fit(BJD, flux, dmag)
+    periods, power = model.periodogram_auto(nyquist_factor)
+    
+    if ('model.optimizer.period_range' in kwargs): model.optimizer.period_range=kwargs['model.optimizer.period_range']
+    else: model.optimizer.period_range=(0.2, 1.4)
+
+    period = model.best_period
+    return period
